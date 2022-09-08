c671f7aa25e395ea38253c4a90fa308da8c5ba77
Vinícius Jardim <viniciuspjardim@gmail.com>
2022-09-08 12:46:19 -0300

    Trying middleware with cookie
    Not able to read the cookie set in the middleware in the first access
    to the page in getServerSideProps. It's a Next.js limitation.

---
+++ middleware.js
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+  console.log("==== Middleware ====", request.url);
+
+  const response = NextResponse.rewrite(new URL("/home", request.url));
+
+  if (request.nextUrl.pathname.startsWith("/experience-a")) {
+    response.cookies.set("experience", "var_A");
+  } else if (request.nextUrl.pathname.startsWith("/experience-b")) {
+    response.cookies.set("experience", "var_B");
+  }
+
+  return response;
+}
+
+export const config = {
+  matcher: ["/experience-a/home", "/experience-b/home"],
+};
