0b5568bb0647de9e855af76e02ce6c0ba1d96690
Vinícius Jardim <viniciuspjardim@gmail.com>
2022-09-10 16:08:03 -0300

    Fix client side page navigation

--- pages/product/a.js
+++ pages/product/a.js
@@ -2,18 +2,18 @@ import Product from "../../components/Product";
 import { brazilTimeString } from "../../helpers/brazilTime";
 
 // Should be cached by Cloudflare
-export default function ProductPageB({ experience, serverTime, nonce }) {
+export default function ProductPageA({ experience, serverTime, nonce }) {
   return (
     <Product experience={experience} serverTime={serverTime} nonce={nonce} />
   );
 }
 
 export async function getServerSideProps() {
-  console.log("==== Product B ====");
+  console.log("==== Product A ====");
 
   return {
     props: {
-      experience: "B",
+      experience: "A",
       serverTime: brazilTimeString(),
       nonce: Math.floor(Math.random() * 1000000),
     },
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,18 +1,24 @@
 import Product from "../../components/Product";
 import { brazilTimeString } from "../../helpers/brazilTime";
 
+// Should NOT be cached by Cloudflare. It's used to client side navigation like
+// using <Link />
 export default function ProductPage({ experience, serverTime, nonce }) {
   return (
     <Product experience={experience} serverTime={serverTime} nonce={nonce} />
   );
 }
 
-export async function getServerSideProps() {
-  console.log("==== Product A ====");
+export async function getServerSideProps({ req }) {
+  console.log(`==== Product (cookie ${req?.cookies?.experience} ) ====`);
+
+  // Get the experience from the cookie. If none is set we can set a default
+  // value.
+  const experience = req?.cookies?.experience ?? "none";
 
   return {
     props: {
-      experience: "A",
+      experience,
       serverTime: brazilTimeString(),
       nonce: Math.floor(Math.random() * 1000000),
     },
